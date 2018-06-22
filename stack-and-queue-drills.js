a1d37d922115a08a06c01d507b9970846dd6338f
Tammy Tian <tiantammy@outlook.com>
2018-06-22 18:25:56 +0200

    in progress, is_palindrome

--- stack-and-queue-drills.js
+++ stack-and-queue-drills.js
@@ -2,6 +2,7 @@
 
 const Stack = require('./stack');
 const starTrek = new Stack();
+const starTrek2 = new Stack();
 
 // const Queue = require('./queue');
 // const queue = new Queue();
@@ -10,8 +11,15 @@ starTrek.push('Spock');
 starTrek.push('McCoy');
 starTrek.push('Scotty');
 
+// starTrek2.push('Kirk');
+// starTrek2.push('Spock');
+// starTrek2.push('McCoy');
+// starTrek2.push('Scotty');
+
+
 console.log(JSON.stringify(starTrek, null, 2));
-const peek = stack => console.log(stack.top.data);
+//const peek = stack => console.log(stack.top.data);
+
 
 const displayStack = stack => {
   if(!stack.top) return null;
@@ -23,10 +31,46 @@ const displayStack = stack => {
   return;
 };
 
+
+//console.log(displayStack(starTrek) === displayStack(starTrek2));
+
 starTrek.pop();
 starTrek.pop();
 starTrek.push('Scotty');
 
 
-peek(starTrek);
-displayStack(starTrek);
+//peek(starTrek);
+//displayStack(starTrek);
+
+
+const is_palindrome = str => {
+  str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  const stack1 = new Stack();
+
+  //create and store display of stack1
+  for (let i = 0; i < str.length; i ++){
+    stack1.push(str[i]);
+  }
+
+
+  const stack2 = new Stack();
+
+  console.log(JSON.stringify(stack1, null, 2));
+  // console.log(stack1.pop());
+  // console.log(stack1.pop());
+  // console.log(stack1.pop());
+
+  // console.log(stack1.pop());
+  // console.log(stack1.pop());
+
+  while(stack1.top) {
+    let popped = stack1.pop();
+    console.log(stack2.push(popped));
+    
+  }
+  ///pop stack1 then push into stack2
+  //compare 'str' to newString created
+
+};
+
+is_palindrome('hello');
