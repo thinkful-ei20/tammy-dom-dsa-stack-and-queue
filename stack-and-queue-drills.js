b4807d12ce79e05c2809f135959b5ac806de3574
Tammy Tian <tiantammy@outlook.com>
2018-06-23 21:08:03 +0200

    working on parens function

--- stack-and-queue-drills.js
+++ stack-and-queue-drills.js
@@ -18,7 +18,7 @@ starTrek.push('Scotty');
 
 
 console.log(JSON.stringify(starTrek, null, 2));
-//const peek = stack => console.log(stack.top.data);
+const peek = stack => stack.top.data;
 
 
 const displayStack = stack => {
@@ -44,7 +44,7 @@ starTrek.push('Scotty');
 
 
 const is_palindrome = str => {
-  str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
   const stack1 = new Stack();
 
   //create and store display of stack1
@@ -66,3 +66,37 @@ const is_palindrome = str => {
 // console.log(is_palindrome('hello')); //false
 // console.log(is_palindrome('dad')); //true 
 // console.log(is_palindrome('A man, a plan, a canal: Panama')); //true
+
+//will take in a string on parens, 
+//if and check if there are pairs
+//if it's incorrect, returns position in the string
+
+//input '((()))' // okay
+//input '()(()' // Extra paren at index 2
+//input '((()))(' //Extra paren at index 5
+//NEED TO FIX PEEK FUNCTION if no top data exists
+const matchingParatheses = (paratheses) =>{
+  const openParens = new Stack();
+  for(let i = 0; i < paratheses.length; i ++){
+    const paren = paratheses[i];
+    if (paren === '(') {
+      openParens.push(paren);
+    } else if  (paren === ')'){
+      let top = openParens.top.data;
+      //check to see if there are anymore parens to pop
+      if (!top){
+        return `There is an extra ')' paren at index ${i+1}`;
+      } 
+      openParens.pop();
+    }    
+    //check to see if there are left over '('
+  }
+  if (openParens.top.data){
+    return 'There is an extra \'(\' paren';
+  }
+
+  return 'No hanging parens';
+
+};
+
+console.log(matchingParatheses('()'));
